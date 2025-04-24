559ef8a43650dd10bd993ad6ae226c159a69ec2e
박유진 <galilee155@naver.com>
2025-04-24 11:11:09 +0900

    hotfix: docker, gitlab api swagger 설정

--- backend/src/main/java/org/example/backend/controller/DockerController.java
+++ backend/src/main/java/org/example/backend/controller/DockerController.java
@@ -1,5 +1,9 @@
 package org.example.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.backend.controller.response.docker.ImageResponse;
@@ -13,13 +17,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/docker")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Docker", description = "도커 API")
 public class DockerController {
 
     private final DockerService dockerService;
 
     @GetMapping("/images")
+    @Operation(summary = "도커 이미지 조회_공식만", security = @SecurityRequirement(name = "JWT"))
     public ResponseEntity<ApiResponse<ImageResponse>> searchDockerImages(
-            @RequestParam String query,
+            @Parameter(description = "검색어 (예: ubuntu, nginx)", example = "redis") @RequestParam String query,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int pageSize
     ) {
@@ -28,9 +34,10 @@ public class DockerController {
     }
 
     @GetMapping("/images/{namespace}/{image}/tags")
+    @Operation(summary = "도커 이미지의 태그 조회", security = @SecurityRequirement(name = "JWT"))
     public ResponseEntity<ApiResponse<TagResponse>> searchDockerImageTags(
-            @PathVariable String namespace,
-            @PathVariable String image,
+            @Parameter(example = "library") @PathVariable String namespace,
+            @Parameter(description = "이미지 이름 (예: redis)", example = "redis") @PathVariable String image,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int pageSize
     ) {
