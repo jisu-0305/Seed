8ad79145fc20c97fbe8e16b225cb90bdbbe12986
0ys <0ysgong@gmail.com>
2025-05-02 12:54:01 +0900

    fix: 서버 렌더링과 클라이언트 훅 관련 빌드 에러 변경 - 서스펜스로 감싸주기

--- frontend/src/app/oauth/callback/page.tsx
+++ frontend/src/app/oauth/callback/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { Suspense } from 'react';
+
 import OAuthCallback from '@/components/Landing/oauth';
 
 export default function OAuthCallbackPage() {
-  return <OAuthCallback />;
+  return (
+    <Suspense>
+      <OAuthCallback />
+    </Suspense>
+  );
 }
 
 export const dynamic = 'force-dynamic';
