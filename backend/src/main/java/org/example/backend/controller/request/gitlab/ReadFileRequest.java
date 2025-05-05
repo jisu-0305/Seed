fa0c42d78a6e0224f04b474b1e6efd5a229e5bc0
박유진 <galilee155@naver.com>
2025-05-06 02:07:56 +0900

    refactor: ReadFileRequest 불필요 어노테이션 제거

--- backend/src/main/java/org/example/backend/controller/request/gitlab/ReadFileRequest.java
+++ backend/src/main/java/org/example/backend/controller/request/gitlab/ReadFileRequest.java
@@ -1,18 +1,15 @@
 package org.example.backend.controller.request.gitlab;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.constraints.NotBlank;
 
 @Schema(name = "FileRequest", description = "파일 원본 조회 요청 DTO")
 public record ReadFileRequest (
-        @NotBlank
-        @Schema(description = "가져올 파일 경로 (예: src/main/java/…)", example = "backend/build.gradle")
-        @RequestParam("filePath")
-        String filePath,
+    @NotBlank
+    @Schema(description = "가져올 파일 경로 (예: src/main/java/…)", example = "backend/build.gradle")
+    String filePath,
 
-        @NotBlank
-        @Schema(description = "가져올 파일의 브랜치 또는 커밋 SHA", example = "dev", defaultValue= "master")
-        @RequestParam(defaultValue = "master")
-        String branch
+    @NotBlank
+    @Schema(description = "가져올 파일의 브랜치 또는 커밋 SHA", example = "dev", defaultValue= "master")
+    String branch
 ){}
