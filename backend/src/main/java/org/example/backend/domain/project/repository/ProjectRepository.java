40ae69eb8b0d7e8877a71b81fac7d7f70de5d8e2
이재훈 <dlwogns1205@naver.com>
2025-05-12 13:24:49 +0900

    feat: .env, .pem 저장 위치 변경 / 파일 정리

--- backend/src/main/java/org/example/backend/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/org/example/backend/domain/project/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package org.example.backend.domain.project.repository;
 import org.example.backend.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectCustomRepository {
+    List<Project> findByIdIn(List<Long> projectIdList);
 }
