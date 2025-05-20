4893004a6754bfc9af493ff35be09d0014919c37
이재훈 <dlwogns1205@naver.com>
2025-05-20 14:42:45 +0900

    feat: DB 데드락 해결

--- backend/src/main/java/org/example/backend/domain/server/service/ServerStatusServiceImpl.java
+++ backend/src/main/java/org/example/backend/domain/server/service/ServerStatusServiceImpl.java
@@ -5,19 +5,15 @@ import org.example.backend.domain.project.entity.Project;
 import org.example.backend.domain.project.enums.ServerStatus;
 import org.example.backend.domain.project.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ServerStatusServiceImpl implements ServerStatusService {
 
-    private final ProjectRepository projectRepository;
-
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     public void updateStatus(Project project, ServerStatus serverStatus) {
         project.updateAutoDeploymentStatus(serverStatus);
-        projectRepository.saveAndFlush(project);
     }
 }
